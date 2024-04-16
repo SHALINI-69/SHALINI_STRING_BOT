544da61540ebd9e9b59d38418dd4d1fd85b4b435
SHALINI <155555305+SHALINI-69@users.noreply.github.com>
2024-04-16 15:25:03 +0530

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -1,15 +1,29 @@
 from pyrogram import filters
 from pyrogram.types import Message
-
+import nekos
 from StringGen import Anony
 from StringGen.utils import add_served_user, keyboard
+import requests
+import random
+import os
+import re
+from os import getenv
+import time
+
+
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002041372224"))
 
 
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
-    await message.reply_text(
-        text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
+    await message.reply_photo(
+        photo=nekos.img("neko"),
+        caption=f"ʜᴇʏ {message.from_user.mention},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
     await add_served_user(message.from_user.id)
+    await Anony.send_message(
+                LOG_GROUP_ID,
+                f"{message.from_user.mention} has just started Bot.\n\n**USER ID:** {message.from_user.id}\n**USER NAME:** {message.from_user.username}",
+        )
